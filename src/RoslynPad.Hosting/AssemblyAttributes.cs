df64020d5abb0f4c7de85939d1aad7badc811bda
amabe <amabe.haruaki@isid.co.jp>
2019-09-26 15:15:56 +0900

    不要な InternalsVisibleTo の設定を削除

--- src/RoslynPad.Hosting/AssemblyAttributes.cs
+++ src/RoslynPad.Hosting/AssemblyAttributes.cs
@@ -2,6 +2,3 @@
 
 [assembly: InternalsVisibleTo("RoslynPad")]
 [assembly: InternalsVisibleTo("RoslynPad.Common.UI")]
-[assembly: InternalsVisibleTo("RoslynPad.Host32")]
-[assembly: InternalsVisibleTo("RoslynPad.Host64")]
-[assembly: InternalsVisibleTo("RoslynPad.HostNetCore")]
