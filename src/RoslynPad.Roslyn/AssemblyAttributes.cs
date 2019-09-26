c1ac57b45d6d1870003cdf4b18de493aef370a22
amabe <amabe.haruaki@isid.co.jp>
2019-09-26 16:03:10 +0900

    ...Windowsと合わせて不要になったフレンドアセンブリの設定を解除

--- src/RoslynPad.Roslyn/AssemblyAttributes.cs
+++ src/RoslynPad.Roslyn/AssemblyAttributes.cs
@@ -2,6 +2,4 @@
 
 [assembly: InternalsVisibleTo("RoslynPad")]
 [assembly: InternalsVisibleTo("RoslynPad.Common.UI")]
-[assembly: InternalsVisibleTo("RoslynPad.Editor.Avalonia")]
-[assembly: InternalsVisibleTo("RoslynPad.Roslyn.Avalonia")]
 [assembly: InternalsVisibleTo("RoslynPad.Hosting")]
